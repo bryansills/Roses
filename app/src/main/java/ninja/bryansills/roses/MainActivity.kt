555e415eb70aa116980fcead375d77fd51d3eb6e
Bryan Sills <bryan@bryansills.com>
2018-07-01 22:35:15 -0500

    Add RxJava to main module

--- app/src/main/java/ninja/bryansills/roses/MainActivity.kt
+++ app/src/main/java/ninja/bryansills/roses/MainActivity.kt
@@ -2,21 +2,39 @@ package ninja.bryansills.roses
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
 import ninja.bryansills.network.NetworkService
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var networkService: NetworkService
+    lateinit var subscription: CompositeDisposable
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         networkService = NetworkService(BuildConfig.FEEDLY_ACCESS_TOKEN)
+        subscription = CompositeDisposable()
     }
 
     override fun onResume() {
         super.onResume()
-        networkService.getProfile()
+        subscription.add(networkService.getProfile()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                        { profile -> Log.d("BLARG", profile.toString()) },
+                        { error -> Log.e("BLARG", error.toString()) }
+                )
+        )
+    }
+
+    override fun onPause() {
+        subscription.clear()
+        super.onPause()
     }
 }
