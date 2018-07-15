f0312cbf442051c8ce2e0f9838a6be9de13f51b0
Bryan Sills <bryan@bryansills.com>
2018-07-15 14:47:53 -0500

    Update to 28 and androidx.*

--- app/src/main/java/ninja/bryansills/roses/MainActivity.kt
+++ app/src/main/java/ninja/bryansills/roses/MainActivity.kt
@@ -1,7 +1,7 @@
 package ninja.bryansills.roses
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.util.Log
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
--- network/src/main/java/ninja/bryansills/network/streams/EntryResponse.kt
+++ network/src/main/java/ninja/bryansills/network/streams/EntryResponse.kt
@@ -1,9 +1,12 @@
 package ninja.bryansills.network.streams
 
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
 data class EntryResponse(
         val id: String,
         val title: String,
         val published: Long,
         val author: String,
-        val canonicalUrl: String
+        val canonicalUrl: String?
 )
