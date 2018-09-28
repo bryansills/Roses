6d283491d5b1d3ebdb1d62a53176be47c966dd90
Bryan Sills <bryan@bryansills.com>
2018-09-27 23:22:23 -0500

    Just extend DaggerApplication

--- app/src/main/java/ninja/bryansills/roses/RosesApplication.kt
+++ app/src/main/java/ninja/bryansills/roses/RosesApplication.kt
@@ -1,20 +1,10 @@
 package ninja.bryansills.roses
 
-import android.app.Activity
-import android.app.Application
-import dagger.android.DispatchingAndroidInjector
-import dagger.android.HasActivityInjector
-import javax.inject.Inject
+import dagger.android.AndroidInjector
+import dagger.android.support.DaggerApplication
 
 
-class RosesApplication : Application(), HasActivityInjector {
-    @Inject lateinit var dispatchingActivityInjector: DispatchingAndroidInjector<Activity>
-
-    override fun onCreate() {
-        super.onCreate()
-
-        DaggerApplicationComponent.builder().create(this)
-    }
-
-    override fun activityInjector() = dispatchingActivityInjector
+class RosesApplication : DaggerApplication() {
+    override fun applicationInjector(): AndroidInjector<out DaggerApplication> =
+            DaggerApplicationComponent.builder().create(this)
 }
