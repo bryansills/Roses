76b71a2f59e4b2284250c82b62fe7a7739d34da8
Bryan Sills <bryan@bryansills.com>
2018-10-22 22:11:30 -0500

    Move Entry screen to Fragment

--- app/src/main/java/ninja/bryansills/roses/entry/EntryFragment.kt
+++ app/src/main/java/ninja/bryansills/roses/entry/EntryFragment.kt
@@ -1,6 +1,8 @@
-package ninja.bryansills.roses.category
+package ninja.bryansills.roses.entry
 
 import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -8,7 +10,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
-import androidx.navigation.findNavController
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -20,33 +21,43 @@ import ninja.bryansills.roses.R
 import ninja.bryansills.roses.ViewModelFactory
 import javax.inject.Inject
 
-class CategoryFragment : Fragment() {
+class EntryFragment : Fragment() {
 
     @Inject lateinit var viewModelFactory: ViewModelFactory
-    lateinit var categoryViewModel: CategoryViewModel
+    lateinit var entryViewModel: EntryViewModel
     lateinit var subscription: CompositeDisposable
 
-    lateinit var categoryAdapter: CategoryAdapter
-    lateinit var categoryList: RecyclerView
+    lateinit var categoryId: String
+    lateinit var entryAdapter: EntryAdapter
+    lateinit var entryList: RecyclerView
 
     override fun onAttach(context: Context) {
         AndroidSupportInjection.inject(this)
         super.onAttach(context)
     }
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        categoryId = EntryFragmentArgs.fromBundle(arguments).categoryId
+    }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view = inflater.inflate(R.layout.fragment_category, container, false)
+        val view = inflater.inflate(R.layout.fragment_entry, container, false)
 
-        categoryList = view.findViewById(R.id.category_list)
-        categoryList.layoutManager = LinearLayoutManager(context)
-        categoryList.addItemDecoration(DividerItemDecoration(context, DividerItemDecoration.VERTICAL))
-        categoryList.adapter = CategoryAdapter {
-            val action = CategoryFragmentDirections.selectCategory()
-            action.setCategoryId(it.id)
-            view.findNavController().navigate(action)
-        }.also { this.categoryAdapter = it }
+        entryList = view.findViewById(R.id.entry_list)
+        entryList.layoutManager = LinearLayoutManager(context)
+        entryList.addItemDecoration(DividerItemDecoration(context, DividerItemDecoration.VERTICAL))
+        entryList.adapter = EntryAdapter {
+            if (it.url != null && activity != null) {
+                val uri = Uri.parse(it.url)
+                val browserIntent = Intent(Intent.ACTION_VIEW, uri)
+                if (browserIntent.resolveActivity(activity!!.packageManager) != null) {
+                    startActivity(browserIntent)
+                }
+            }
+        }.also { this.entryAdapter = it }
 
-        categoryViewModel = ViewModelProviders.of(this, viewModelFactory)[CategoryViewModel::class.java]
+        entryViewModel = ViewModelProviders.of(this, viewModelFactory)[EntryViewModel::class.java]
         subscription = CompositeDisposable()
 
         return view
@@ -54,11 +65,11 @@ class CategoryFragment : Fragment() {
 
     override fun onStart() {
         super.onStart()
-        subscription.add(categoryViewModel.categories()
+        subscription.add(entryViewModel.getEntries(categoryId)
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(
-                        { response -> categoryAdapter.submitList(response) },
+                        { response -> entryAdapter.submitList(response) },
                         { error -> Log.w("BLARG", error.toString()) }
                 ))
     }
--- app/src/main/java/ninja/bryansills/roses/inject/FragmentBuildersModule.kt
+++ app/src/main/java/ninja/bryansills/roses/inject/FragmentBuildersModule.kt
@@ -3,9 +3,13 @@ package ninja.bryansills.roses.inject
 import dagger.Module
 import dagger.android.ContributesAndroidInjector
 import ninja.bryansills.roses.category.CategoryFragment
+import ninja.bryansills.roses.entry.EntryFragment
 
 @Module
 abstract class FragmentBuildersModule {
     @ContributesAndroidInjector(modules = [ViewModelModule::class])
     abstract fun contributeCategoryFragment(): CategoryFragment
+
+    @ContributesAndroidInjector(modules = [ViewModelModule::class])
+    abstract fun contributeEntryFragment(): EntryFragment
 }
