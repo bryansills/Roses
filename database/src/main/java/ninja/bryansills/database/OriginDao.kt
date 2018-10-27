d5f71432ff9a9a2ee01f7252e66f40703b0cece1
Bryan Sills <bryan@bryansills.com>
2018-10-27 12:24:17 -0500

    Actually upsert origins

--- database/src/main/java/ninja/bryansills/database/OriginDao.kt
+++ database/src/main/java/ninja/bryansills/database/OriginDao.kt
@@ -2,9 +2,30 @@ package ninja.bryansills.database
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Transaction
+import androidx.room.Update
 
 @Dao
 interface OriginDao {
-    @Insert()
+    @Query("SELECT * FROM origins WHERE network_id = :networkId LIMIT 1")
+    fun getOrigin(networkId: String): Origin?
+
+    @Insert
     fun insertOrigin(origin: Origin): Long
+
+    @Update
+    fun updateOrigin(origin: Origin): Int
+
+    @Transaction
+    fun upsertOrigin(origin: Origin): Long {
+        val existingOrigin = getOrigin(origin.networkId)
+
+        return if (existingOrigin != null) {
+            updateOrigin(origin.copy(id = existingOrigin.id))
+            existingOrigin.id ?: -1
+        } else {
+            insertOrigin(origin)
+        }
+    }
 }
