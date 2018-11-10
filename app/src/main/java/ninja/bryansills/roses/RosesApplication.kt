19a6cd814c7a8fd88e23049ae650169a5e7f5be9
Bryan Sills <bryan@bryansills.com>
2018-11-10 16:25:18 -0600

    Get a very basic WorkManager request working

--- app/src/main/java/ninja/bryansills/roses/RosesApplication.kt
+++ app/src/main/java/ninja/bryansills/roses/RosesApplication.kt
@@ -1,11 +1,32 @@
 package ninja.bryansills.roses
 
+import androidx.work.Constraints
+import androidx.work.NetworkType
+import androidx.work.PeriodicWorkRequest
+import androidx.work.WorkManager
 import dagger.android.AndroidInjector
 import dagger.android.support.DaggerApplication
+import ninja.bryansills.background.BackgroundWorker
 import ninja.bryansills.roses.inject.DaggerApplicationComponent
+import java.util.concurrent.TimeUnit
 
 
 class RosesApplication : DaggerApplication() {
+    override fun onCreate() {
+        super.onCreate()
+
+        val constraints = Constraints.Builder()
+                .setRequiredNetworkType(NetworkType.CONNECTED)
+                .setRequiresBatteryNotLow(true)
+                .build()
+        val request = PeriodicWorkRequest
+                .Builder(BackgroundWorker::class.java, 15, TimeUnit.MINUTES)
+                .addTag("BLARG")
+                .setConstraints(constraints)
+                .build()
+        WorkManager.getInstance().enqueue(request)
+    }
+
     override fun applicationInjector(): AndroidInjector<out DaggerApplication> =
             DaggerApplicationComponent.builder().create(this)
 }
--- background/src/main/java/ninja/bryansills/background/BackgroundWorker.kt
+++ background/src/main/java/ninja/bryansills/background/BackgroundWorker.kt
@@ -4,19 +4,17 @@ import android.content.Context
 import android.util.Log
 import androidx.work.Worker
 import androidx.work.WorkerParameters
-import io.reactivex.schedulers.Schedulers
-import ninja.bryansills.repo.Repository
 
 class BackgroundWorker(context: Context,
-                       workerParams: WorkerParameters,
-                       private val repository: Repository)
+                       workerParams: WorkerParameters)
     : Worker(context, workerParams) {
 
     override fun doWork(): Result {
         return try {
-            repository.updateDatabase()
-                    .subscribeOn(Schedulers.io())
-                    .blockingAwait()
+//            repository.updateDatabase()
+//                    .subscribeOn(Schedulers.io())
+//                    .blockingAwait()
+            Log.d("BLARG", "we here!")
             Result.SUCCESS
         } catch (throwable: Throwable) {
             Log.d("BLARG", throwable.message)
