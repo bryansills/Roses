ae487bc81a7f98c8cd3889ca597b25f70360dd20
Bryan Sills <bryan@bryansills.com>
2018-11-11 15:53:35 -0600

    Do not create duplicate BackgroundWorkers and also probably go a little too far with Dagger

--- app/src/main/java/ninja/bryansills/roses/inject/ApplicationModule.kt
+++ app/src/main/java/ninja/bryansills/roses/inject/ApplicationModule.kt
@@ -5,9 +5,14 @@ import dagger.Module
 import dagger.Provides
 import ninja.bryansills.background.WorkerModule
 import ninja.bryansills.roses.RosesApplication
+import javax.inject.Named
 
 @Module(includes = [RepoModule::class, ViewModelModule::class, WorkerModule::class])
 class ApplicationModule {
     @Provides
     fun context(app: RosesApplication): Context = app.applicationContext
+
+    @Provides
+    @Named("REFRESH_INTERVAL")
+    fun provideRefreshInterval(): Int = 3
 }
--- background/src/main/java/ninja/bryansills/background/WorkerModule.kt
+++ background/src/main/java/ninja/bryansills/background/WorkerModule.kt
@@ -1,13 +1,18 @@
 package ninja.bryansills.background
 
 import android.content.Context
-import androidx.work.WorkManager
+import androidx.work.Constraints
+import androidx.work.ExistingPeriodicWorkPolicy
+import androidx.work.NetworkType
+import androidx.work.PeriodicWorkRequest
 import androidx.work.WorkerFactory
 import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import dagger.multibindings.ClassKey
 import dagger.multibindings.IntoMap
+import java.util.concurrent.TimeUnit
+import javax.inject.Named
 
 @Module(includes = [WorkerModule.Bindings::class])
 class WorkerModule {
@@ -22,13 +27,34 @@ class WorkerModule {
         fun bindBackgroundWorkerFactory(workerFactory: BackgroundWorkerFactory): RosesWorkerFactory
     }
 
-        @Provides
-        fun workerManager(context: Context, workerFactory: WorkerFactory)
-                = WorkerManager(context, workerFactory)
+    @Provides
+    fun workerManager(
+            context: Context,
+            workerFactory: WorkerFactory,
+            @Named("BACKGROUND_WORK_NAME") workName: String,
+            workPolicy: ExistingPeriodicWorkPolicy,
+            workRequest: PeriodicWorkRequest
+    ) = WorkerManager(context, workerFactory, workName, workPolicy, workRequest)
 
-//    @Provides
-//    fun backgroundWorkerConstraints(): Constraints = Constraints.Builder()
-//            .setRequiredNetworkType(NetworkType.CONNECTED)
-//            .setRequiresBatteryNotLow(true)
-//            .build()
+    @Provides
+    fun backgroundWorkerConstraints(): Constraints = Constraints.Builder()
+            .setRequiredNetworkType(NetworkType.CONNECTED)
+            .setRequiresBatteryNotLow(true)
+            .build()
+
+    @Provides
+    @Named("BACKGROUND_WORK_NAME")
+    fun backgroundWorkName(): String = "BACKGROUND_WORKER"
+
+    @Provides
+    fun existingWorkPolicy(): ExistingPeriodicWorkPolicy = ExistingPeriodicWorkPolicy.KEEP
+
+    @Provides
+    fun backgroundWorkRequest(
+            constraints: Constraints,
+            @Named("REFRESH_INTERVAL") refreshInterval: Int
+    ): PeriodicWorkRequest = PeriodicWorkRequest
+            .Builder(BackgroundWorker::class.java, refreshInterval.toLong(), TimeUnit.HOURS)
+            .setConstraints(constraints)
+            .build()
 }
