7ee12fa85731a0822030d7f21a758c43ddb0454b
Bryan Sills <bryan@bryansills.com>
2018-11-24 13:46:19 -0600

    Integrate network module more with Dagger

--- network/src/main/java/ninja/bryansills/network/NetworkModule.kt
+++ network/src/main/java/ninja/bryansills/network/NetworkModule.kt
@@ -2,6 +2,11 @@ package ninja.bryansills.network
 
 import dagger.Module
 import dagger.Provides
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.moshi.MoshiConverterFactory
 import javax.inject.Named
 import javax.inject.Singleton
 
@@ -9,6 +14,37 @@ import javax.inject.Singleton
 class NetworkModule {
     @Provides
     @Singleton
-    fun network(@Named("FEEDLY_ACCESS_TOKEN") feedlyAccessToken: String): NetworkService =
-            NetworkService(feedlyAccessToken)
+    fun network(feedlyService: FeedlyService): NetworkService =
+            NetworkService(feedlyService)
+
+    @Provides
+    @Singleton
+    fun authInterceptor(@Named("FEEDLY_ACCESS_TOKEN") feedlyAccessToken: String): Interceptor =
+            Interceptor { chain ->
+                chain.proceed(chain.request()
+                        .newBuilder()
+                        .addHeader("Authorization", feedlyAccessToken)
+                        .build())
+            }
+
+    @Provides
+    @Singleton
+    fun okhttp(interceptor: Interceptor): OkHttpClient =
+            OkHttpClient.Builder()
+                .addInterceptor(interceptor)
+                .build()
+
+    @Provides
+    @Singleton
+    fun retrofit(okHttpClient: OkHttpClient): Retrofit =
+            Retrofit.Builder()
+                .baseUrl("https://cloud.feedly.com/v3/")
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .addConverterFactory(MoshiConverterFactory.create().asLenient())
+                .client(okHttpClient)
+                .build()
+
+    @Provides
+    @Singleton
+    fun feedlyService(retrofit: Retrofit): FeedlyService = retrofit.create(FeedlyService::class.java)
 }
--- network/src/main/java/ninja/bryansills/network/NetworkService.kt
+++ network/src/main/java/ninja/bryansills/network/NetworkService.kt
@@ -2,33 +2,9 @@ package ninja.bryansills.network
 
 import io.reactivex.Observable
 import ninja.bryansills.network.streams.StreamContentsResponse
-import okhttp3.Interceptor
-import okhttp3.OkHttpClient
-import retrofit2.Retrofit
-import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
-import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Inject
 
-class NetworkService(feedlyAccessToken: String) {
-    private val feedly: FeedlyService
-    init {
-        val interceptor = Interceptor { chain ->
-            chain.proceed(chain.request()
-                    .newBuilder()
-                    .addHeader("Authorization", feedlyAccessToken)
-                    .build())
-        }
-        val okHttp = OkHttpClient.Builder()
-                .addInterceptor(interceptor)
-                .build()
-        val retrofit = Retrofit.Builder()
-                .baseUrl("https://cloud.feedly.com/v3/")
-                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-                .addConverterFactory(MoshiConverterFactory.create().asLenient())
-                .client(okHttp)
-                .build()
-        feedly = retrofit.create(FeedlyService::class.java)
-    }
-
+class NetworkService @Inject constructor(val feedly: FeedlyService) {
     fun streamContents(): Observable<StreamContentsResponse> {
         return feedly.profile().flatMap { response ->
             feedly.streamContents("user/${response.id}/category/global.all", 50)
