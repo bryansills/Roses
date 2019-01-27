24a725716d1ea5440792cf347bf1eb892bf21e5f
Bryan Sills <bryan@bryansills.com>
2019-01-27 14:36:18 -0600

    Move network-models module to using buildSrc dependencies

---
+++ buildSrc/src/main/kotlin/Libs.kt
@@ -0,0 +1,10 @@
+object Libs {
+    // Core
+    const val kotlin_stdlib = "org.jetbrains.kotlin:kotlin-stdlib:" + Versions.kotlin
+    const val dagger = "com.google.dagger:dagger:" + Versions.dagger
+    const val dagger_compiler = "com.google.dagger:dagger-compiler:" + Versions.dagger
+
+    // Square
+    const val moshi = "com.squareup.moshi:moshi:" + Versions.moshi
+    const val moshi_codegen = "com.squareup.moshi:moshi-kotlin-codegen:" + Versions.moshi
+}
---
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -0,0 +1,8 @@
+object Versions {
+    // Core
+    const val kotlin = "1.3.11"
+    const val dagger = "2.16" // available: "2.21"
+
+    // Square
+    const val moshi = "1.8.0"
+}
