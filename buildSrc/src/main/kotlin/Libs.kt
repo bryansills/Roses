d038bc8bff6e537f923d90460c05c86cfabc01cc
Bryan Sills <bryan@bryansills.com>
2019-01-27 18:04:18 -0600

    Migrate more modules to buildSrc dependencies

--- buildSrc/src/main/kotlin/Libs.kt
+++ buildSrc/src/main/kotlin/Libs.kt
@@ -1,8 +1,18 @@
 object Libs {
+    // Buildscript
+    const val gradle_versions_plugin = "com.github.ben-manes:gradle-versions-plugin:" + Versions.gradle_versions_plugin
+    const val android_gradle_plugin = "com.android.tools.build:gradle:" + Versions.android_gradle_plugin
+    const val kotlin_gradle_plugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:" + Versions.kotlin
+    const val navigation_safe_args_gradle_plugin = "android.arch.navigation:navigation-safe-args-gradle-plugin:" + Versions.navigation
+
     // Core
     const val kotlin_stdlib = "org.jetbrains.kotlin:kotlin-stdlib:" + Versions.kotlin
     const val dagger = "com.google.dagger:dagger:" + Versions.dagger
     const val dagger_compiler = "com.google.dagger:dagger-compiler:" + Versions.dagger
+
+    // Android
+    const val room_runtime = "androidx.room:room-runtime:" + Versions.room
+    const val room_compiler = "androidx.room:room-compiler:" + Versions.room
 
     // Square
     const val moshi = "com.squareup.moshi:moshi:" + Versions.moshi
--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,7 +1,20 @@
 object Versions {
+    // Buildscript
+    const val gradle_versions_plugin = "0.20.0"
+    const val android_gradle_plugin = "3.3.0"
+
+    // Android SDK
+    const val compile_sdk = 28
+    const val min_sdk = 24
+    const val target_sdk = 28
+
     // Core
-    const val kotlin = "1.3.11"
+    const val kotlin = "1.3.0" // available: "1.3.20"
     const val dagger = "2.16" // available: "2.21"
+
+    // Android
+    const val room = "2.1.0-alpha02"
+    const val navigation = "1.0.0-alpha07"
 
     // Square
     const val moshi = "1.8.0"
