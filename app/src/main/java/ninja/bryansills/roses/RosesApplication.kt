879fda07171f1b6fa6af8b37a28f8cf691ac29b4
Bryan Sills <bryan@bryansills.com>
2019-02-01 08:34:58 -0600

    Temporarily disable the worker

--- app/src/main/java/ninja/bryansills/roses/RosesApplication.kt
+++ app/src/main/java/ninja/bryansills/roses/RosesApplication.kt
@@ -2,14 +2,12 @@ package ninja.bryansills.roses
 
 import dagger.android.AndroidInjector
 import dagger.android.support.DaggerApplication
-import ninja.bryansills.background.WorkerManager
 import ninja.bryansills.roses.inject.DaggerApplicationComponent
-import javax.inject.Inject
 
 
 class RosesApplication : DaggerApplication() {
 
-    @Inject lateinit var workerManager: WorkerManager
+//    @Inject lateinit var workerManager: WorkerManager
 
     override fun applicationInjector(): AndroidInjector<out DaggerApplication> =
             DaggerApplicationComponent.builder().create(this)
--- app/src/main/java/ninja/bryansills/roses/inject/ApplicationComponent.kt
+++ app/src/main/java/ninja/bryansills/roses/inject/ApplicationComponent.kt
@@ -3,14 +3,13 @@ package ninja.bryansills.roses.inject
 import dagger.Component
 import dagger.android.AndroidInjector
 import dagger.android.support.AndroidSupportInjectionModule
-import ninja.bryansills.background.WorkerComponent
 import ninja.bryansills.repo.RepoComponent
 import ninja.bryansills.roses.RosesApplication
 import javax.inject.Singleton
 
 @Singleton
 @Component(
-        dependencies = [RepoComponent::class, WorkerComponent::class],
+        dependencies = [RepoComponent::class],
         modules = [
             AndroidSupportInjectionModule::class,
             ApplicationModule::class,
@@ -18,5 +17,7 @@ import javax.inject.Singleton
 ])
 interface ApplicationComponent: AndroidInjector<RosesApplication> {
     @Component.Builder
-    abstract class Builder : AndroidInjector.Builder<RosesApplication>()
+    abstract class Builder : AndroidInjector.Builder<RosesApplication>() {
+        abstract fun repoComponent(repoComponent: RepoComponent): Builder
+    }
 }
