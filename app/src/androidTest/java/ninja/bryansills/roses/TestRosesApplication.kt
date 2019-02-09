0acadd4ec2958b405d1fd3580af0e38c30273c0a
Bryan Sills <bryan@bryansills.com>
2019-02-09 17:13:10 -0600

    Revert "Tests do not require a WorkerComponent"
    This reverts commit f976444c0bfdf92de5faaa2fd7311d181200e43a.

--- app/src/androidTest/java/ninja/bryansills/roses/TestRosesApplication.kt
+++ app/src/androidTest/java/ninja/bryansills/roses/TestRosesApplication.kt
@@ -3,8 +3,10 @@ package ninja.bryansills.roses
 import dagger.android.AndroidInjector
 import dagger.android.support.DaggerApplication
 import ninja.bryansills.roses.inject.DaggerFakeRepoComponent
+import ninja.bryansills.roses.inject.DaggerFakeWorkerComponent
 import ninja.bryansills.roses.inject.DaggerTestApplicationComponent
 import ninja.bryansills.roses.inject.FakeRepoModule
+import ninja.bryansills.roses.inject.FakeWorkerModule
 
 class TestRosesApplication : DaggerApplication() {
     override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
@@ -13,8 +15,14 @@ class TestRosesApplication : DaggerApplication() {
                 .fakeRepoModule(fakeRepoModule)
                 .build()
 
+        val fakeWorkerModule = FakeWorkerModule()
+        val fakeWorkerComponent = DaggerFakeWorkerComponent.builder()
+                .fakeWorkerModule(fakeWorkerModule)
+                .build()
+
         return DaggerTestApplicationComponent.builder()
                 .fakeRepoComponent(fakeRepoComponent)
+                .fakeWorkerComponent(fakeWorkerComponent)
                 .build()
     }
 }
--- app/src/androidTest/java/ninja/bryansills/roses/inject/TestApplicationComponent.kt
+++ app/src/androidTest/java/ninja/bryansills/roses/inject/TestApplicationComponent.kt
@@ -3,19 +3,20 @@ package ninja.bryansills.roses.inject
 import dagger.Component
 import dagger.android.AndroidInjector
 import dagger.android.support.AndroidSupportInjectionModule
-import ninja.bryansills.roses.TestRosesApplication
+import ninja.bryansills.roses.RosesApplication
 
 @Component(
-        dependencies = [FakeRepoComponent::class],
+        dependencies = [FakeWorkerComponent::class, FakeRepoComponent::class],
         modules = [
             AndroidSupportInjectionModule::class,
             ApplicationModule::class,
             ActivityModule::class
         ]
 )
-interface TestApplicationComponent : AndroidInjector<TestRosesApplication> {
+interface TestApplicationComponent : AndroidInjector<RosesApplication> {
         @Component.Builder
-        abstract class Builder : AndroidInjector.Builder<TestRosesApplication>() {
+        abstract class Builder : AndroidInjector.Builder<RosesApplication>() { // AndroidInjector<RosesApplication>???
             abstract fun fakeRepoComponent(fakeRepoComponent: FakeRepoComponent): Builder
+            abstract fun fakeWorkerComponent(fakeWorkerComponent: FakeWorkerComponent): Builder
         }
 }
