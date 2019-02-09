f976444c0bfdf92de5faaa2fd7311d181200e43a
Bryan Sills <bryan@bryansills.com>
2019-02-09 17:02:43 -0600

    Tests do not require a WorkerComponent

--- app/src/androidTest/java/ninja/bryansills/roses/TestRosesApplication.kt
+++ app/src/androidTest/java/ninja/bryansills/roses/TestRosesApplication.kt
@@ -3,10 +3,8 @@ package ninja.bryansills.roses
 import dagger.android.AndroidInjector
 import dagger.android.support.DaggerApplication
 import ninja.bryansills.roses.inject.DaggerFakeRepoComponent
-import ninja.bryansills.roses.inject.DaggerFakeWorkerComponent
 import ninja.bryansills.roses.inject.DaggerTestApplicationComponent
 import ninja.bryansills.roses.inject.FakeRepoModule
-import ninja.bryansills.roses.inject.FakeWorkerModule
 
 class TestRosesApplication : DaggerApplication() {
     override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
@@ -15,14 +13,8 @@ class TestRosesApplication : DaggerApplication() {
                 .fakeRepoModule(fakeRepoModule)
                 .build()
 
-        val fakeWorkerModule = FakeWorkerModule()
-        val fakeWorkerComponent = DaggerFakeWorkerComponent.builder()
-                .fakeWorkerModule(fakeWorkerModule)
-                .build()
-
         return DaggerTestApplicationComponent.builder()
                 .fakeRepoComponent(fakeRepoComponent)
-                .fakeWorkerComponent(fakeWorkerComponent)
                 .build()
     }
 }
