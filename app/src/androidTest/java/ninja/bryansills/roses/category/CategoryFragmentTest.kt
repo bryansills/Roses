18825e6588e8daec333038345a2f4fe725b0c72a
Bryan Sills <bryan@bryansills.com>
2019-03-03 15:46:58 -0600

    Add test for category RecyclerView items

--- app/src/androidTest/java/ninja/bryansills/roses/category/CategoryFragmentTest.kt
+++ app/src/androidTest/java/ninja/bryansills/roses/category/CategoryFragmentTest.kt
@@ -10,13 +10,16 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModelProvider
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.hasDescendant
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import ninja.bryansills.repo.Category
 import ninja.bryansills.roses.R
 import ninja.bryansills.roses.utils.FragmentScenarioRule
 import ninja.bryansills.roses.utils.SingleViewModelFactory
+import ninja.bryansills.roses.utils.atPosition
 import ninja.bryansills.roses.utils.onDataBindingFragment
 import org.hamcrest.Matchers.not
 import org.junit.Before
@@ -66,6 +69,27 @@ class CategoryFragmentTest {
         scenario.onDataBindingFragment {
             categoryViewModel.categories.value = CategoryUiModel.Success(emptyList())
         }
+
+        onView(withId(R.id.category_list)).check(matches(not(isDisplayed())))
+        onView(withId(R.id.loading_bar)).check(matches(not(isDisplayed())))
+        onView(withId(R.id.category_empty)).check(matches(withText(R.string.categories_empty)))
+    }
+
+    @Test
+    fun displayList() {
+        scenario.onDataBindingFragment {
+            val categories = listOf(
+                    Category("1", "FIRST", 5),
+                    Category("2", "SECOND", 7)
+            )
+            categoryViewModel.categories.value = CategoryUiModel.Success(categories)
+        }
+
+        onView(withId(R.id.category_list)).check(matches(atPosition(0, hasDescendant(withText("FIRST")))))
+        onView(withId(R.id.category_list)).check(matches(atPosition(0, hasDescendant(withText("5")))))
+
+        onView(withId(R.id.category_list)).check(matches(atPosition(1, hasDescendant(withText("SECOND")))))
+        onView(withId(R.id.category_list)).check(matches(atPosition(1, hasDescendant(withText("7")))))
     }
 }
 
